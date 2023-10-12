4a1a007d9329bc8ea5def4f0930dd4b75020c770
Fredrik S <fredrik.spannar@gmail.com>
2023-10-12 20:43:29 +0200

    Added Laravel Breeze with scaffolding

--- README.md
+++ README.md
@@ -13,10 +13,18 @@ docker run --name auctionit-postgres -e POSTGRES_PASSWORD=laravel -d postgres
 ( just make sure Docker/Docker Desktop is installed and running on the computer )
 
 ### backend and frontend
-To start up local web-server and frontend (Vite) with HMR:
+To start up backend:
 
 ```
-php artisan serve && npm run vite
+php artisan serve
 ```
+
+To start react frontend:
+
+```
+npm run dev
+```
+
+You probably need to run backend and frontend in two different prompts/terminal tabs
 
 ... then you can visit: http://localhost:8000/
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,15 +1,21 @@
 import './bootstrap';
+import '../css/app.css';
 
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import {createInertiaApp } from '@inertiajs/inertia-react'
-import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    // Below you can see that we are going to get all React components from resources/js/Pages folder
-    // @ts-ignore
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
     },
-})
+    progress: {
+        color: '#4B5563',
+    },
+});
