abbd5ca3cbe318cfa4bd017b2c7fbe138574a077
Fredrik S <fredrik.spannar@gmail.com>
2023-10-12 20:48:57 +0200

    Relationships for model Item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+
+    /**
+     * Get the seller associated with the item.
+     */
+    public function seller(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'seller_id');
+    }
+
+    /**
+     * Get the buyer associated with the item.
+     */
+    public function buyer(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'buyer_id');
+    }
 }
