b79e3e340ef6a62a954e201d5c7e74136b7760f9
Fredrik S <fredrik.spannar@gmail.com>
2023-10-12 11:35:48 +0200

    merge batch file spaces to stars

--- merge-production-with-development.bat
+++ merge-production-with-development.bat
@@ -1,10 +1,11 @@
 @echo off
 git checkout production && git merge development && git push origin production
 
-echo ""
-echo "... All done! Press spacebar to go back to branch development"
+echo ***********************************************************************************
+echo All done! Press spacebar to go back to branch development
 pause
 
 git checkout development
-echo ""
+
+echo ***********************************************************************************
 pause
